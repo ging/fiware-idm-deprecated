30665bf835c4ce3e8e2db4e4419697fad087ea39
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-10-05 15:13:08 +0200

    Verification script changed

--- extras/scripts/idm-verification.sh
+++ extras/scripts/idm-verification.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# After a few seconds, you should see some HTML from the landing page
+# After a few seconds, you should get a 200 OK response
 # This means both Keystone back-end and Horizon front-end are correctly installed and working
 
-curl http://localhost:8000
+wget http://localhost:8000
