298a010edb30ff62c9fc18ecd507663600f6a80b
Enrique Garcia Navalon <garcianavalon@gmail.com>
2015-10-06 13:19:00 +0200

    RTD theme in local too

--- doc/conf.py
+++ doc/conf.py
@@ -121,7 +121,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     html_theme = 'default'
 else:
-    html_theme = 'alabaster'
+    html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
