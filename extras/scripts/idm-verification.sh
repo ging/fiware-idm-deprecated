666520d22bcb6426e50880fd1204db6e6a27ffee
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-10-21 18:31:33 +0200

    Improved verification script

--- extras/scripts/idm-verification.sh
+++ extras/scripts/idm-verification.sh
@@ -3,4 +3,4 @@
 # After a few seconds, you should get a 200 OK response
 # This means both Keystone back-end and Horizon front-end are correctly installed and working
 
-wget http://localhost:8000
+wget http://127.0.0.1:8000
