fe02938e76c6e9918e76c17f4b6776eec1effd38
aalonsog <aalonsog@dit.upm.es>
2015-11-17 12:57:11 +0100

    First commuit

--- extras/scripts/idm-verification.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# After a few seconds, you should get a 200 OK response and the welcoming index.html file
-# This means both Keystone back-end and Horizon front-end are correctly installed and working
-
-wget http://$IP:8000 -O /dev/null
