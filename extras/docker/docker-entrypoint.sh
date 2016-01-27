ce950da342026db2244e7d3481f39fb5770e8cf6
FedericoFdez <fa.fernandez@alumnos.upm.es>
2016-01-27 14:15:49 +0100

    Updated Dockerfile (Now working for development environment)

--- extras/docker/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start Keystone as a Service, run Horizon using dev_server
-source /usr/local/bin/virtualenvwrapper.sh
-workon idm_tools
-fab keystone.dev_server &
-fab horizon.dev_server
