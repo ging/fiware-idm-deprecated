508ba2867950cd349db69957f2fcb9f17b938aea
FedericoFdez <fefernandez@dit.upm.es>
2016-02-05 14:35:49 +0100

    Updated IdM installation script to meet the new installation process

--- extras/scripts/idm-installation.sh
+++ extras/scripts/idm-installation.sh
@@ -3,53 +3,82 @@
 # Install Ubuntu dependencies
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-sudo apt-get install -y wget python python-dev git
+sudo apt-get install -y wget python git vim expect
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 
-# Install virtualenvwrapper and virtualenv
-sudo pip install virtualenvwrapper
-export WORKON_HOME=~/venvs
-mkdir -p $WORKON_HOME
-
-echo "
-# Settings for VirtualenvWrapper
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Devel
-source /usr/local/bin/virtualenvwrapper.sh
-" >> .profile
-source ~/.profile
-
-echo "
-# Settings for VirtualenvWrapper
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Devel
-source /usr/local/bin/virtualenvwrapper.sh
-" >> .bashrc
-source ~/.bashrc
+# Install Ubuntu project dependencies
+sudo apt-get install -y python-dev python-virtualenv libssl-dev libffi-dev libjpeg8-dev libxml2-dev libxslt1-dev libsasl2-dev libssl-dev libldap2-dev libffi-dev libsqlite3-dev libmysqlclient-dev python-mysqldb
 
 # Download latest version of the code 
-git clone https://github.com/ging/fiware-idm idm
-cd idm
-cp conf/settings.py.example conf/settings.py
+git clone https://github.com/ging/keystone
+(cd keystone && git checkout tags/keyrock-5.1.0)
 
-# Create virtualenv
-mkvirtualenv idm_tools
-workon idm_tools
+git clone https://github.com/ging/horizon
+(cd horizon && git checkout tags/keyrock-5.1.0)
+
+# Configuring settings files
+cp keystone/etc/keystone.conf.sample keystone/etc/keystone.conf
+ cp horizon/openstack_dashboard/local/local_settings.py.example horizon/openstack_dashboard/local/local_settings.py
+sed -i s/\$\$IDM_PASS/idm/g horizon/openstack_dashboard/local/local_settings.py
 
 # Install python dependecies
-pip install -r requirements.txt
+sudo python keystone/tools/install_venv.py
+sudo python horizon/tools/install_venv.py
+
+# Sync database
+sudo keystone/tools/with_venv.sh keystone/bin/keystone-manage db_sync
+sudo keystone/tools/with_venv.sh keystone/bin/keystone-manage db_sync --extension=endpoint_filter
+sudo keystone/tools/with_venv.sh keystone/bin/keystone-manage db_sync --extension=oauth2
+sudo keystone/tools/with_venv.sh keystone/bin/keystone-manage db_sync --extension=roles
+sudo keystone/tools/with_venv.sh keystone/bin/keystone-manage db_sync --extension=user_registration
+sudo keystone/tools/with_venv.sh keystone/bin/keystone-manage db_sync --extension=two_factor_auth
+
+# Set up idm password
+wget https://raw.githubusercontent.com/ging/fiware-idm/master/extras/docker/expect_idm_password
+chmod +x expect_idm_password
+sudo ./expect_idm_password
+
+# Set up environment variables
+ABSOLUTE_KEYROCK_PATH="$(pwd)"
+ABSOLUTE_HORIZON_PATH="$(echo $ABSOLUTE_KEYROCK_PATH/horizon)"
+ABSOLUTE_KEYSTONE_PATH="$(echo $ABSOLUTE_KEYROCK_PATH/keystone)"
 
 # Install Keystone back-end and set-up service
-fab keystone.install
-fab keystone.database_create
-fab keystone.set_up_as_service
+/bin/cat <<EOM >/etc/init/keystone_idm.conf
+# keystone_idm - keystone_idm job file
+ description "Service conf file for the IdM backend based in Keystone"
+ author "Enrique Garcia Navalon <garcianavalon@gmail.com>"
+ start on (local-filesystems and net-device-up IFACE!=lo)
+ stop on runlevel [016]
+# Automatically restart process if crashed
+respawn
+setuid root
+script
+cd $ABSOLUTE_KEYSTONE_PATH
+#activate the venv
+. .venv/bin/activate
+#run keystone
+bin/keystone-all
+end script
+EOM
 sudo service keystone_idm start
-sleep 10s
-fab keystone.populate
-sleep 10s
 
 # Install Horizon front-end and set-up service
-fab horizon.install:unattended=True
-fab horizon.set_up_as_service
+/bin/cat <<EOM >/etc/init/horizon_idm.conf
+# horizon_idm - horizon_idm job file
+description "Service conf file for the IdM frontend based in Horizon"
+start on (local-filesystems and net-device-up IFACE!=lo)
+stop on runlevel [016]
+# Automatically restart process if crashed
+respawn
+setuid root
+script
+cd $ABSOLUTE_HORIZON_PATH
+#activate the venv
+. .venv/bin/activate
+#run horizon
+python manage.py runserver 0.0.0.0:8000
+end script
+EOM
 sudo service horizon_idm start
