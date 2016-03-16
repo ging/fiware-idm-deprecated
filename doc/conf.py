0af2da05ae809b2cd9011a05c974b2884063551b
Álvaro Alonso <aalonsog@dit.upm.es>
2016-03-16 17:44:58 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -120,14 +120,14 @@ import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     html_theme = 'default'
-else:
-    html_theme = 'sphinx_rtd_theme'
     html_context = {
         'css_files': [
             'resources/fiware_style.css',
         ],
     }
-
+else:
+    html_theme = 'sphinx_rtd_theme'
+    
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
