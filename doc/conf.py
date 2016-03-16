bb289e2a0b09e47baec931025b0c1b62049ceaa8
Álvaro Alonso <aalonsog@dit.upm.es>
2016-03-16 17:39:53 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -122,6 +122,11 @@ if on_rtd:
     html_theme = 'default'
 else:
     html_theme = 'sphinx_rtd_theme'
+    html_context = {
+        'css_files': [
+            'doc/resources/fiware_style.css',
+        ],
+    }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
